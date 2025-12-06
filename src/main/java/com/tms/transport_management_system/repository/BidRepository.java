bcc0f3593dd7bb9f795578f97f54afae77764684
ayush2442 <ayushpandey2442@gmail.com>
2025-12-07 00:03:50 +0530

    Repository - Modified BidRepository

--- src/main/java/com/tms/transport_management_system/repository/BidRepository.java
+++ src/main/java/com/tms/transport_management_system/repository/BidRepository.java
@@ -2,6 +2,7 @@ package com.tms.transport_management_system.repository;
 
 import com.tms.transport_management_system.entity.Bid;
 import com.tms.transport_management_system.enums.BidStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface BidRepository {
+public interface BidRepository extends JpaRepository<Bid, UUID> {
 
     List<Bid> findByLoadId(UUID loadId);
 
